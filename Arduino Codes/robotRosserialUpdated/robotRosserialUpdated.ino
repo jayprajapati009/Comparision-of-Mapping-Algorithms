ba5c63ce95da8a05f84cddf5e32d33b474570b6c
jayprajapati009 <jayprajapati9900@gmail.com>
2022-03-31 16:40:08 +0530

    ROS Serial Code Revision 1

--- Arduino Codes/robotRosserialUpdated/robotRosserialUpdated.ino
+++ Arduino Codes/robotRosserialUpdated/robotRosserialUpdated.ino
@@ -2,17 +2,20 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 
-#define EN_L 5
+
+#define EN_L 9
 #define IN1_L 10
 #define IN2_L 11
-#define EN_R 6
+
+#define EN_R 8
 #define IN1_R 12
 #define IN2_R 13
+
 
 double w_r = 0, w_l = 0;
 
 //wheel_rad is the wheel radius ,wheel_sep is
-double wheel_rad = 0.0325, wheel_sep = 0.185;
+double wheel_rad = 0.0325, wheel_sep = 0.295;
 
 
 ros::NodeHandle nh;
@@ -34,74 +37,123 @@ void MotorL(int Pulse_Width1);
 void MotorR(int Pulse_Width2);
 
 void setup() {
+
   Motors_init();
+
   nh.initNode();
+
   nh.subscribe(sub);
+
 }
 
 
 void loop() {
-  MotorL(w_l * 10);
-  MotorR(w_r * 10);
+  MotorL(w_l * 1);
+
+  MotorR(w_r * 1);
+
   nh.spinOnce();
+
 }
 
 
 void Motors_init() {
+
   pinMode(EN_L, OUTPUT);
+
   pinMode(EN_R, OUTPUT);
+
   pinMode(IN1_L, OUTPUT);
+
   pinMode(IN2_L, OUTPUT);
+
   pinMode(IN1_R, OUTPUT);
+
   pinMode(IN2_R, OUTPUT);
 
   digitalWrite(EN_L, LOW);
+
   digitalWrite(EN_R, LOW);
+
   digitalWrite(IN1_L, LOW);
+
   digitalWrite(IN2_L, LOW);
+
   digitalWrite(IN1_R, LOW);
+
   digitalWrite(IN2_R, LOW);
+
 }
 
 void MotorL(int Pulse_Width1) {
   if (Pulse_Width1 > 0) {
+
     analogWrite(EN_L, Pulse_Width1);
+
     digitalWrite(IN1_L, HIGH);
+
     digitalWrite(IN2_L, LOW);
+
   }
 
   if (Pulse_Width1 < 0) {
+
     Pulse_Width1 = abs(Pulse_Width1);
+
     analogWrite(EN_L, Pulse_Width1);
+
     digitalWrite(IN1_L, LOW);
+
     digitalWrite(IN2_L, HIGH);
+
   }
 
   if (Pulse_Width1 == 0) {
+
     analogWrite(EN_L, Pulse_Width1);
+
     digitalWrite(IN1_L, LOW);
+
     digitalWrite(IN2_L, LOW);
+
   }
+
 }
 
 
 void MotorR(int Pulse_Width2) {
+
+
   if (Pulse_Width2 > 0) {
+
     analogWrite(EN_R, Pulse_Width2);
-    digitalWrite(IN1_R, HIGH);
-    digitalWrite(IN2_R, LOW);
+
+    digitalWrite(IN1_R, LOW);
+
+    digitalWrite(IN2_R, HIGH);
+
   }
 
   if (Pulse_Width2 < 0) {
+
     Pulse_Width2 = abs(Pulse_Width2);
+
     analogWrite(EN_R, Pulse_Width2);
-    digitalWrite(IN1_R, LOW);
-    digitalWrite(IN2_R, HIGH);
+
+    digitalWrite(IN1_R, HIGH);
+
+    digitalWrite(IN2_R, LOW);
+
   }
 
   if (Pulse_Width2 == 0) {
+
     analogWrite(EN_R, Pulse_Width2);
-    digitalWrite(IN1_R, HIGH);
-    digitalWrite(IN2_R, HIGH);
+
+    digitalWrite(IN1_R, LOW);
+
+    digitalWrite(IN2_R, LOW);
+
   }
+
 }
